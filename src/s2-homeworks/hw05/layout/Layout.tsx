cb9427b8d19a5f20f8846df8362c405d2e9175c1
vladseme2012 <vlad.seme2012@yandex.ru>
2025-03-01 21:04:01 +0300

    fixed styles and return in component Layout

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -3,8 +3,6 @@ import {Header} from '../header/Header';
 import {Sidebar} from '../sidebar/Sidebar';
 import {PATH} from '../Pages';
 import {useLocation} from 'react-router-dom';
-import s from './Layout.module.css';
-
 
 type PropsType = {
     children: ReactNode
@@ -15,17 +13,6 @@ export const Layout: FC<PropsType> = ({children}) => {
     const handleClose = () => setOpen(false);
     const handleOpen = () => setOpen(true);
 
-    const location = useLocation();
-    const currentPath = location.pathname;
-
-    const pageName =
-        currentPath === PATH.PRE_JUNIOR
-            ? 'Pre-junior'
-            : currentPath === PATH.JUNIOR
-                ? 'Junior'
-                : currentPath === PATH.JUNIOR_PLUS
-                    ? 'Junior Plus'
-                    : 'Error';
     useEffect(() => {
         open && (document.body.style.overflow = 'hidden');
         !open && (document.body.style.overflow = 'unset');
@@ -33,10 +20,10 @@ export const Layout: FC<PropsType> = ({children}) => {
 
     return (
         <>
-            {!open ? (<Header handleOpen={handleOpen}/>) : (<Sidebar open={open} handleClose={handleClose}/>)}
+            <Sidebar open={open} handleClose={handleClose}/>
+            <Header handleOpen={handleOpen}/>
             <div>
                 {/*страницы*/}
-                {open ? (<h1 className={s.title}>{pageName}</h1>) : (<></>)}
                 {children}
             </div>
         </>
