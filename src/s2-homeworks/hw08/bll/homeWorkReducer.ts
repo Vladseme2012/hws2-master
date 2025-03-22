76890b4937d3203f757bd3de2058a6bbbc8f0a08
vladseme2012 <vlad.seme2012@yandex.ru>
2025-03-22 15:47:52 +0300

    resolved task hv08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,18 @@
-import {UserType} from '../HW8'
+import {UserType} from '../HW8';
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            return action.payload === 'up'
+                ? ([...state.sort((a, b) => a.name.localeCompare(b.name))])
+                : [...state.sort((a, b) => b.name.localeCompare(a.name))];
         }
         case 'check': {
-
-            return state // need to fix
+            return [...state.filter(u => u.age >= 18)];
         }
         default:
-            return state
+            return state;
     }
-}
+};
