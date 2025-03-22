c002cbc5f3fea8d3743eb17fbd88b3f7765b85bf
vladseme2012 <vlad.seme2012@yandex.ru>
2025-03-22 16:39:51 +0300

    fixed reducer in case immutable sort initial array

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -6,11 +6,11 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': {
             return action.payload === 'up'
-                ? ([...state.sort((a, b) => a.name.localeCompare(b.name))])
-                : [...state.sort((a, b) => b.name.localeCompare(a.name))];
+                ? ([...state].sort((a, b) => a.name.localeCompare(b.name)))
+                : ([...state].sort((a, b) => b.name.localeCompare(a.name)));
         }
         case 'check': {
-            return [...state.filter(u => u.age >= 18)];
+            return state.filter(u => u.age >= 18);
         }
         default:
             return state;
