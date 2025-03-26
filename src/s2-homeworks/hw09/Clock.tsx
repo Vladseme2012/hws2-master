ad163c6f6ca9cf7c94a9ec51c8fced06c52a4365
vladseme2012 <vlad.seme2012@yandex.ru>
2025-03-26 21:25:54 +0300

    fixed function in component Clock

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -1,39 +1,46 @@
-import React, {useState} from 'react'
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import {restoreState} from '../hw06/localStorage/localStorage'
-import s from './Clock.module.css'
+import React, {useState} from 'react';
+import moment from 'moment';
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton';
+import {restoreState} from '../hw06/localStorage/localStorage';
+import s from './Clock.module.css';
 
 function Clock() {
-    const [timerId, setTimerId] = useState<number | undefined>(undefined)
+    const [timerId, setTimerId] = useState<number | undefined>(undefined);
     // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
-    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
-    const [show, setShow] = useState<boolean>(false)
+    const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())));
+    const [show, setShow] = useState<boolean>(false);
 
     const start = () => {
+        const id: number = window.setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+        setTimerId(id);
 
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
 
-    }
+    };
 
     const stop = () => {
+        clearInterval(timerId);
+        setTimerId(undefined);
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
 
-    }
+    };
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-
-    }
+        setShow(true);
+    };
     const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
+        setShow(false);
+    };
 
-    }
-
-    const stringTime = 'date->time' || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = moment().format('hh:mm:ss') || <br/>;
+    const stringDate = moment().format('DD.MM.YYYY') || <br/>;
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = 'date->day' || <br/> // пишут студенты
-    const stringMonth = 'date->month' || <br/> // пишут студенты
+    const stringDay = moment().format('dddd') || <br/>; // пишут студенты
+    const stringMonth = moment().format('MMMM') || <br/>; // пишут студенты
 
     return (
         <div className={s.clock}>
@@ -67,7 +74,7 @@ function Clock() {
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={true} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={false} // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                 >
                     start
@@ -81,7 +88,7 @@ function Clock() {
                 </SuperButton>
             </div>
         </div>
-    )
+    );
 }
 
-export default Clock
+export default Clock;
